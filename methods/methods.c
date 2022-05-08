2a1273435baa9541e3ac4013f3e8a09010f3c395
mohamed sa3ed <mohame.sa3ed.faried@gmail.com>
2022-05-08 22:18:49 +0200

    updating methods

---
+++ methods/methods.c
@@ -0,0 +1,16 @@
+#include "methods.h"
+#include"stdio.h"
+int Mode()
+{
+  printf("Welcome to SCS \n \
+--------------------------------------\n \
+0 for admin mode \n \
+1 for user mode \n");
+int x ;
+scanf("%d",&x);
+return x ;
+}
+int Login () 
+{
+    
+}
---
+++ methods/methods.h
@@ -0,0 +1,9 @@
+#ifndef METHODS.H
+#define METHODS.H
+
+int  Mode() ;
+int  Login();
+void DisplayUserOptions();
+void DisplayAdminOptions();
+
+#endif
