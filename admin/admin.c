d6bfe7cf71c12234d26c000ea6f6ec2411737907
mohamed sa3ed <mohame.sa3ed.faried@gmail.com>
2022-05-10 23:51:42 +0200

    add function

--- admin/admin.c
+++ admin/admin.c
@@ -1,3 +1,16 @@
 #include <stdlib.h>
 #include "admin.h"
 #include "../user/user.h"
+
+void AddStudent (int id ,int password,int score, Student *ps)
+{
+    StudentNode *p;
+    p=(StudentNode*)malloc(sizeof (StudentNode));
+    p->id=id;
+   // p->name = name ;
+    p->password = password ;
+    p->score =score ;
+    p->next =ps->top ;
+    ps->top=p;
+    ps->numberOfStudents ++;
+}
