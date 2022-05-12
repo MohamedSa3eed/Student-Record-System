26bacd7986932ed146be89fda9a6e2a3db6abe53
yasmeen <yasmeenm322@gmail.com>
2022-05-13 01:45:44 +0200

    user functions

--- user/user.c
+++ user/user.c
@@ -1,3 +1,81 @@
 #include <stdio.h>
 #include "user.h"
+//not tested yet
+void ViewRecord(Student * ps)
+{
+    while(1)
+    {
+     int id,i,state=0;
+     printf("enter your id\n");
+     scanf("%d",&id);
+     StudentNode * p = ps->top ;
+     for (i=0;i<ps->numberOfStudents;i++)
+        {
+            if (id == p->id )
+            {
+                state =1;
+                printf("%s",p->name);
+                printf("%d",p->id);
+                printf("%d",p->score);
+            }
+            p=p->next ;
+        }
+        if(state== 0)
+        {
+            printf("enter a valid id\n");
+            continue;
+        }
+        break;
+    }
+}
+void EditPassword (Student* ps)
+{
+     while (1)
+    {
+        int id,i,state = 0;
+        printf("enter your id\n");
+        scanf("%d",&id);
+        StudentNode *p = ps->top ;
+        for (i=0;i<ps->numberOfStudents;i++)
+        {
+            if (id == p->id )
+            {
+                state= 1;
+                scanf("%s",p->password);
+            }
+            p=p->next ;
+        }
+        if(state == 0)
+        {
+            printf("enter a valid id\n");
+            continue;
+        }
+        break;
+    }
+}
 
+void EditName(Student* ps)
+{
+     while (1)
+    {
+        int id,i,state = 0;
+        printf("enter your id\n");
+        scanf("%d",&id);
+        StudentNode *p = ps->top ;
+        for (i=0;i<ps->numberOfStudents;i++)
+        {
+            if (id == p->id )
+            {
+                state= 1;
+                scanf("%s",p->name);
+            }
+            p=p->next ;
+        }
+        if(state == 0)
+        {
+            printf("enter a valid id\n");
+            continue;
+        }
+        break;
+    }
+}
